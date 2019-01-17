0c8a889528f73bf876673c2f7fed1cabe4a1b9cf
AlanWilms <alan.p.wilms@gmail.com>
2019-01-17 13:58:00 -0600

    add test

---
+++ closest_face_detection_camera2.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# Copyright 2017 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Camera inference face detection demo code.
+
+Runs continuous face detection on the VisionBonnet and prints the number of
+detected faces.
+
+Example:
+face_detection_camera.py --num_frames 10
+"""
+import argparse
+
+from aiy.vision.inference import CameraInference
+from aiy.vision.models import face_detection
+from examples.vision.annotator import Annotator
+from picamera import PiCamera
+
+
+def main():
+    """Face detection camera inference example."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--num_frames',
+        '-n',
+        type=int,
+        dest='num_frames',
+        default=-1,
+        help='Sets the number of frames to run for, otherwise runs forever.')
+    args = parser.parse_args()
+
+    with PiCamera() as camera:
+        # Forced sensor mode, 1640x1232, full FoV. See:
+        # https://picamera.readthedocs.io/en/release-1.13/fov.html#sensor-modes
+        # This is the resolution inference run on.
+        camera.sensor_mode = 4
+
+        # Scaled and cropped resolution. If different from sensor mode implied
+        # resolution, inference results must be adjusted accordingly. This is
+        # true in particular when camera.start_recording is used to record an
+        # encoded h264 video stream as the Pi encoder can't encode all native
+        # sensor resolutions, or a standard one like 1080p may be desired.
+        camera.resolution = (1640, 1232)
+
+        # Start the camera stream.
+        camera.framerate = 30
+        camera.start_preview()
+
+if __name__ == '__main__':
+    main()
